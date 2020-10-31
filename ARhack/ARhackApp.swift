657d51af1063929fa4c4a38a3b846f2b0d398360
Alongkot Intaragumhang <alonkkot.non@gmail.com>
2020-10-31 11:05:42 +0700

    Initial Commit

---
+++ ARhack/ARhackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARhackApp.swift
+//  ARhack
+//
+//  Created by alongkot on 31/10/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct ARhackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARhack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ARhack
+//
+//  Created by alongkot on 31/10/2563 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
