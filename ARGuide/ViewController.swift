a0ca5d8c9f77aff501ae1fff41eba3b172a8a9cc
Alongkot Intaragumhang <alonkkot.non@gmail.com>
2020-11-01 23:11:19 +0700

    add model

---
+++ ARGuide/ViewController.swift
@@ -0,0 +1,205 @@
+//
+//  ViewController.swift
+//  ARWalkthrough
+//
+//  Created by Wyszynski, Daniel on 2/16/18.
+//  Copyright © 2018 Nike, Inc. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    var sceneController = HoverScene()
+
+    var didInitializeScene: Bool = false
+    var planes = [ARPlaneAnchor: Plane]()
+    let feedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
+    var visibleGrid: Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        //sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+
+        // Create a new scene
+        if let scene = sceneController.scene {
+            // Set the scene to the view
+            sceneView.scene = scene
+        }
+    
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTapScreen))
+        tapRecognizer.numberOfTapsRequired = 1
+        tapRecognizer.numberOfTouchesRequired = 1
+        self.view.addGestureRecognizer(tapRecognizer)
+        
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didDoubleTapScreen))
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.numberOfTouchesRequired = 1
+        self.view.addGestureRecognizer(doubleTapRecognizer)
+        
+        feedbackGenerator.prepare()
+    }
+    
+    
+    @objc func didPinchScreen(recognizer: UIGestureRecognizer) {
+        if didInitializeScene {
+         }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal]
+
+        sceneView.session.delegate = self
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    @objc func didDoubleTapScreen(recognizer: UITapGestureRecognizer) {
+        if didInitializeScene {
+            self.visibleGrid = !self.visibleGrid
+            planes.forEach({ (_, plane) in
+                plane.setPlaneVisibility(self.visibleGrid)
+            })
+        }
+    }
+    
+    @objc func didTapScreen(recognizer: UITapGestureRecognizer) {
+        if didInitializeScene {
+            if let camera = sceneView.session.currentFrame?.camera {
+                let tapLocation = recognizer.location(in: sceneView)
+                let hitTestResults = sceneView.hitTest(tapLocation)
+                if let node = hitTestResults.first?.node, let scene = sceneController.scene {
+                    if let sphere = node.topmost(until: scene.rootNode) as? Sphere {
+                        sphere.animate()
+                    } else if let plane = node.parent as? Plane, let planeParent = plane.parent, let hitResult = hitTestResults.first {
+                        let textPos = SCNVector3Make(
+                            hitResult.worldCoordinates.x,
+                            hitResult.worldCoordinates.y,
+                            hitResult.worldCoordinates.z
+                        )
+                        sceneController.addText(string: "DevDisrupt", parent: planeParent, position: textPos)
+                    }
+                }
+                else {
+                    var translation = matrix_identity_float4x4
+                    translation.columns.3.z = -5.0
+                    let transform = camera.transform * translation
+                    let position = SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+                    sceneController.addSphere(position: position)
+                }
+            }
+        }
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            if let planeAnchor = anchor as? ARPlaneAnchor {
+                self.addPlane(node: node, anchor: planeAnchor)
+                self.feedbackGenerator.impactOccurred()
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            if let planeAnchor = anchor as? ARPlaneAnchor {
+                self.updatePlane(anchor: planeAnchor)
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+
+    }
+    
+
+    func addPlane(node: SCNNode, anchor: ARPlaneAnchor) {
+        let plane = Plane(anchor)
+        planes[anchor] = plane
+        plane.setPlaneVisibility(self.visibleGrid)
+
+        node.addChildNode(plane)
+        print("Added plane: \(plane)")
+    }
+    
+    func updatePlane(anchor: ARPlaneAnchor) {
+        if let plane = planes[anchor] {
+            plane.update(anchor)
+        }
+    }
+    
+    func removePlane(anchor: ARPlaneAnchor) {
+        if let plane = planes.removeValue(forKey: anchor) {
+            plane.removeFromParentNode()
+        }
+    }
+
+
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if let camera = sceneView.session.currentFrame?.camera {
+            didInitializeScene = true
+            
+            let transform = camera.transform
+            let position = SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+            sceneController.makeUpdateCameraPos(towards: position)
+        }
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# AR Tutorial Part 3 - Hit detection, planes and text
+
+In this final post of the series, we take a look at how to detect planes, touches within them, and how to place items on them. Follow the [tutorial at the Nike S23NYC Engineering blog](https://medium.com/s23nyc-tech).
+
+
+<img src="https://github.com/AbovegroundDan/ARTutorial_Part3/blob/master/tutorial3.gif" width="20%" height="20%">
+
+# Requirements
+
+- Xcode 9.x+
+- iOS 11+
+- An iOS device with an A9 or better processor (iPhone 6s or superior, iPad Pro, iPad 2017+)
+
+# Installation
+1. Clone this repo.
+2. Open the project in Xcode.
+4. Run the project in a real device with iOS 11 or above. AR can't work in the simulator, so you'll need to configure a team to be able to run your app on a device.
+
+# License
+MIT
